f3dbed272f518db7c80c591994abcf41ee7175c1
wasim <kwasimakram2014@gmail.com>
2022-04-28 12:14:04 +0530

    First commit

---
+++ src/test/java/cucumber/DemoProjects/Firstclass.java
@@ -0,0 +1,11 @@
+package cucumber.DemoProjects;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello1");
+          System.out.println("Hello2");
+	}
+
+}
